987c05f7b01206a58cb49aa8089a858377418b43
strujilloc <3881022596@edu.k.u-tokyo.ac.jp>
2020-03-10 17:59:04 +0900

    install mongoose and make a connnection

---
+++ test/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+// connect ot mongodb
+mongoose.connect("mongodb://localhost/testaroo");
+
+mongoose.connection
+  .once("open", function() {
+    console.log("connection has been made, now make fireworks...");
+  })
+  .on("error", function(error) {
+    console.log("connection error", error);
+  });
